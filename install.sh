058f9c4d041ce79e05833f38b2b054848ba2c053
Evan McQuinn <evan.mcquinn@noaa.gov>
2026-01-19 10:54:40 -0700

    fix checkout backup

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ EOF
     echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
   fi
 
   # dotfiles settings
